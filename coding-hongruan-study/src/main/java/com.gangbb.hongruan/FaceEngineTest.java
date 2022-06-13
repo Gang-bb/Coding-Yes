9511197b4314a60d0f3f8541e214a1ca2fe8abfa
lyx <949526365@qq.com>
2022-06-13 16:14:03 +0800

    feat：对接虹软测试

---
+++ coding-hongruan-study/src/main/java/com.gangbb.hongruan/FaceEngineTest.java
@@ -0,0 +1,194 @@
+package com.gangbb.hongruan;
+
+import com.arcsoft.face.*;
+import com.arcsoft.face.enums.*;
+import com.arcsoft.face.toolkit.ImageInfo;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.arcsoft.face.toolkit.ImageInfoEx;
+
+import static com.arcsoft.face.toolkit.ImageFactory.getGrayData;
+import static com.arcsoft.face.toolkit.ImageFactory.getRGBData;
+
+/**
+ * 人脸识别测试
+ * 用于生活照之间的特征比对，推荐阈值0.80
+ * 用于证件照或生活照与证件照之间的特征比对，推荐阈值0.82
+ *
+ * @author Gangbb
+ * @date 2022/6/8
+ **/
+public class FaceEngineTest {
+
+
+    public static void main(String[] args) {
+
+        //从官网获取
+        String appId = "2bnRw2dunRPZ1snwsjGYmYFspNxcsXbiB5aaJDqknEPf";
+        String sdkKey = "FqZYwQfHSsNqjnMUSLseJMpgkZRtS4utW9nbB2iDeUXp";
+
+
+        FaceEngine faceEngine = new FaceEngine("P:\\idea\\Coding-Yes\\coding-hongruan-study\\lib\\WIN64");
+        //激活引擎
+        int errorCode = faceEngine.activeOnline(appId, sdkKey);
+
+        if (errorCode != ErrorInfo.MOK.getValue() && errorCode != ErrorInfo.MERR_ASF_ALREADY_ACTIVATED.getValue()) {
+            System.out.println("引擎激活失败");
+        }
+
+        // 获取SDK版本信息
+        VersionInfo version = faceEngine.getVersion();
+        System.out.println("SDK版本信息：" + version);
+
+
+
+
+        ActiveFileInfo activeFileInfo = new ActiveFileInfo();
+        errorCode = faceEngine.getActiveFileInfo(activeFileInfo);
+        if (errorCode != ErrorInfo.MOK.getValue() && errorCode != ErrorInfo.MERR_ASF_ALREADY_ACTIVATED.getValue()) {
+            System.out.println("获取激活文件信息失败");
+        }
+        System.out.println("激活文件信息：" + activeFileInfo);
+
+        //引擎配置
+        EngineConfiguration engineConfiguration = new EngineConfiguration();
+        engineConfiguration.setDetectMode(DetectMode.ASF_DETECT_MODE_IMAGE);
+        engineConfiguration.setDetectFaceOrientPriority(DetectOrient.ASF_OP_ALL_OUT);
+        engineConfiguration.setDetectFaceMaxNum(10);
+        engineConfiguration.setDetectFaceScaleVal(16);
+        //功能配置
+        FunctionConfiguration functionConfiguration = new FunctionConfiguration();
+        // 是否支持年龄检测功能
+        functionConfiguration.setSupportAge(true);
+        // 是否支持3D检测功能
+        functionConfiguration.setSupportFace3dAngle(true);
+        // 是否支持人脸检测功能
+        functionConfiguration.setSupportFaceDetect(true);
+        // 是否支持人脸识别功能
+        functionConfiguration.setSupportFaceRecognition(true);
+        // 是否支持性别检测功能
+        functionConfiguration.setSupportGender(true);
+        // 是否支持RGB活体检测功能
+        functionConfiguration.setSupportLiveness(true);
+        // 是否支持IR活体检测功能
+        functionConfiguration.setSupportIRLiveness(true);
+        engineConfiguration.setFunctionConfiguration(functionConfiguration);
+
+
+        //初始化引擎
+        errorCode = faceEngine.init(engineConfiguration);
+
+        if (errorCode != ErrorInfo.MOK.getValue()) {
+            System.out.println("初始化引擎失败");
+        }
+
+
+
+        //人脸检测
+        ImageInfo imageInfo = getRGBData(new File("C:\\Users\\lenovo\\Desktop\\多人2.jpg"));
+        List<FaceInfo> faceInfoList = new ArrayList<FaceInfo>();
+        errorCode = faceEngine.detectFaces(imageInfo.getImageData(), imageInfo.getWidth(), imageInfo.getHeight(), imageInfo.getImageFormat(), faceInfoList);
+        System.out.println(faceInfoList);
+
+        //特征提取
+        FaceFeature faceFeature = new FaceFeature();
+        errorCode = faceEngine.extractFaceFeature(imageInfo.getImageData(), imageInfo.getWidth(), imageInfo.getHeight(), imageInfo.getImageFormat(), faceInfoList.get(0), faceFeature);
+        System.out.println("特征值大小：" + faceFeature.getFeatureData().length);
+        System.out.println("特征值：" + Arrays.toString(faceFeature.getFeatureData()));
+
+        //人脸检测2
+        ImageInfo imageInfo2 = getRGBData(new File("C:\\Users\\lenovo\\Desktop\\测试3.jpg"));
+        List<FaceInfo> faceInfoList2 = new ArrayList<FaceInfo>();
+        errorCode = faceEngine.detectFaces(imageInfo2.getImageData(), imageInfo2.getWidth(), imageInfo2.getHeight(),imageInfo.getImageFormat(), faceInfoList2);
+        System.out.println(faceInfoList);
+
+        //特征提取2
+        FaceFeature faceFeature2 = new FaceFeature();
+        errorCode = faceEngine.extractFaceFeature(imageInfo2.getImageData(), imageInfo2.getWidth(), imageInfo2.getHeight(), imageInfo.getImageFormat(), faceInfoList2.get(0), faceFeature2);
+        System.out.println("特征值大小：" + faceFeature.getFeatureData().length);
+
+        //特征比对
+        FaceFeature targetFaceFeature = new FaceFeature();
+        targetFaceFeature.setFeatureData(faceFeature.getFeatureData());
+        FaceFeature sourceFaceFeature = new FaceFeature();
+        sourceFaceFeature.setFeatureData(faceFeature2.getFeatureData());
+        FaceSimilar faceSimilar = new FaceSimilar();
+
+        errorCode = faceEngine.compareFaceFeature(targetFaceFeature, sourceFaceFeature, faceSimilar);
+
+        System.out.println("相似度：" + faceSimilar.getScore());
+
+        //设置活体测试
+        errorCode = faceEngine.setLivenessParam(0.5f, 0.7f);
+        //人脸属性检测
+        FunctionConfiguration configuration = new FunctionConfiguration();
+        configuration.setSupportAge(true);
+        configuration.setSupportFace3dAngle(true);
+        configuration.setSupportGender(true);
+        configuration.setSupportLiveness(true);
+        errorCode = faceEngine.process(imageInfo.getImageData(), imageInfo.getWidth(), imageInfo.getHeight(), imageInfo.getImageFormat(), faceInfoList, configuration);
+
+
+        //性别检测
+        List<GenderInfo> genderInfoList = new ArrayList<GenderInfo>();
+        errorCode = faceEngine.getGender(genderInfoList);
+        System.out.println("性别：" + genderInfoList.get(0).getGender());
+
+        //年龄检测
+        List<AgeInfo> ageInfoList = new ArrayList<AgeInfo>();
+        errorCode = faceEngine.getAge(ageInfoList);
+        System.out.println("年龄：" + ageInfoList.get(0).getAge());
+
+        //3D信息检测
+        List<Face3DAngle> face3DAngleList = new ArrayList<Face3DAngle>();
+        errorCode = faceEngine.getFace3DAngle(face3DAngleList);
+        System.out.println("3D角度：" + face3DAngleList.get(0).getPitch() + "," + face3DAngleList.get(0).getRoll() + "," + face3DAngleList.get(0).getYaw());
+
+        //活体检测
+        List<LivenessInfo> livenessInfoList = new ArrayList<LivenessInfo>();
+        errorCode = faceEngine.getLiveness(livenessInfoList);
+        System.out.println("活体：" + livenessInfoList.get(0).getLiveness());
+
+
+        //IR属性处理
+        ImageInfo imageInfoGray = getGrayData(new File("C:\\Users\\lenovo\\Desktop\\测试3.jpg"));
+        List<FaceInfo> faceInfoListGray = new ArrayList<FaceInfo>();
+        errorCode = faceEngine.detectFaces(imageInfoGray.getImageData(), imageInfoGray.getWidth(), imageInfoGray.getHeight(), imageInfoGray.getImageFormat(), faceInfoListGray);
+
+        FunctionConfiguration configuration2 = new FunctionConfiguration();
+        configuration2.setSupportIRLiveness(true);
+        errorCode = faceEngine.processIr(imageInfoGray.getImageData(), imageInfoGray.getWidth(), imageInfoGray.getHeight(), imageInfoGray.getImageFormat(), faceInfoListGray, configuration2);
+        //IR活体检测
+        List<IrLivenessInfo> irLivenessInfo = new ArrayList<>();
+        errorCode = faceEngine.getLivenessIr(irLivenessInfo);
+        System.out.println("IR活体：" + irLivenessInfo.get(0).getLiveness());
+
+        ImageInfoEx imageInfoEx = new ImageInfoEx();
+        imageInfoEx.setHeight(imageInfo.getHeight());
+        imageInfoEx.setWidth(imageInfo.getWidth());
+        imageInfoEx.setImageFormat(imageInfo.getImageFormat());
+        imageInfoEx.setImageDataPlanes(new byte[][]{imageInfo.getImageData()});
+        imageInfoEx.setImageStrides(new int[]{imageInfo.getWidth() * 3});
+        List<FaceInfo> faceInfoList1 = new ArrayList<>();
+        errorCode = faceEngine.detectFaces(imageInfoEx, DetectModel.ASF_DETECT_MODEL_RGB, faceInfoList1);
+
+        FunctionConfiguration fun = new FunctionConfiguration();
+        fun.setSupportAge(true);
+        errorCode = faceEngine.process(imageInfoEx, faceInfoList1, functionConfiguration);
+        List<AgeInfo> ageInfoList1 = new ArrayList<>();
+        int age = faceEngine.getAge(ageInfoList1);
+        System.out.println("年龄：" + ageInfoList1.get(0).getAge());
+
+        FaceFeature feature = new FaceFeature();
+        errorCode = faceEngine.extractFaceFeature(imageInfoEx, faceInfoList1.get(0), feature);
+
+
+        //引擎卸载
+        errorCode = faceEngine.unInit();
+
+    }
+}
