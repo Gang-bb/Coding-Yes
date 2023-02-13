f268cfa9b2763c5e34b0a13f02feb270fdd26826
lyx <949526365@qq.com>
2023-02-13 16:31:36 +0800

    LocalTimed的api测试

--- coding-hutool-study/src/main/java/date/Test2.java
+++ coding-hutool-study/src/main/java/date/Test2.java
@@ -3,6 +3,7 @@ package date;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,8 +17,10 @@ import java.util.List;
 public class Test2 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        DateTime parse = DateUtil.parse("2023-01-01 00:02:00", "yyyy-mm");
-        boolean before = parse.isBefore(new Date());
+        DateTime parse = DateUtil.parse("2023-01-01 00:02:00", "yyyy-MM-dd HH:mm");
+        boolean before = parse.isAfter(new Date());
+        LocalDateTime of = LocalDateTime.of(2022, 2, 24, 1, 1);
+        System.out.println(of.isAfter(LocalDateTime.now()));
         System.out.println(parse);
 //        DateTime dateTime = DateUtil.beginOfMonth(DateUtil.now());
 //        System.out.println(dateTime);
